26e6a68ada4fb728c184e88ee8596799d1d03c82
Ryan Currie <ryan.currie@gmail.com>
2017-10-27 15:08:24 -0700

    Fixed tests to be asynchronous. #2

--- test/index.test.js
+++ test/index.test.js
@@ -20,19 +20,24 @@ describe('Continuous Delivery', () => {
   })
 
   describe('Get /', () => {
-    it('Should return a status 200', () => {
+    it('Should return a status 200', done => {
       request('http://localhost:3000', (err, response, body) => {
         expect(err).to.equal(null)
         expect(response.statusCode).to.equal(200)
+        done()
       })
     })
-    it('Should return an object with then name continuous-delivery and a desciption', () => {
-      request('http://localhost:3000', (err, response, body) => {
-        expect(response.body)
-          .to.be.an('object')
-          .that.has.property('name', 'continuous-delivery')
-        expect(response.body).to.have.property('description')
-      })
+    it('Should return an object with then name continuous-delivery and a desciption', done => {
+      request(
+        { url: 'http://localhost:3000', json: true },
+        (err, response, body) => {
+          expect(body)
+            .to.be.an('object')
+            .that.has.property('name', 'continuous-delivery')
+          expect(body).to.have.property('description')
+          done()
+        }
+      )
     })
   })
 })
