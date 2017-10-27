5fcc1fb6f0b1882ffd70a02adafb249fb2eb8d6b
Ryan Currie <ryan.currie@gmail.com>
2017-10-27 14:05:54 -0700

    Added tests for server behavior and added coverage script. #1

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,38 @@
-const { describe, it } = require('mocha')
+const { describe, before, after, it } = require('mocha')
 const { expect } = require('chai')
 const request = require('request')
 const createApp = require('../create-app')
+
+describe('Continuous Delivery', () => {
+  const app = createApp()
+  let server
+
+  before(done => {
+    server = app.listen(3000, () => {
+      done()
+    })
+  })
+
+  after(done => {
+    server.close(() => {
+      done()
+    })
+  })
+
+  describe('Get /', () => {
+    it('Should return a status 200', () => {
+      request('http://localhost:3000', (err, response, body) => {
+        expect(err).to.equal(null)
+        expect(response.statusCode).to.equal(200)
+      })
+    })
+    it('Should return an object with then name continuous-delivery and a desciption', () => {
+      request('http://localhost:3000', (err, response, body) => {
+        expect(response.body)
+          .to.be.an('object')
+          .that.has.property('name', 'continuous-delivery')
+        expect(response.body).to.have.property('description')
+      })
+    })
+  })
+})
