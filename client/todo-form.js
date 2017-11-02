7996e192e9028f9d3e3e43c66df400de9e56ba24
Ryan Currie <ryan.currie@gmail.com>
2017-11-02 16:04:09 -0700

    Set up watch scripts and added event handlers to form. #8

--- client/todo-form.js
+++ client/todo-form.js
@@ -4,21 +4,76 @@ export default class TodoForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: null
+      task: '',
+      dueDate: ''
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const data = {
+      task: formData.get('enter-task'),
+      dueDate: formData.get('enter-date')
+    }
+    console.log(JSON.stringify(data, null, 2))
+    event.target.reset()
+  }
+
+  handleChange({ target }) {
+    const name = target.name
+    const value = target.value
+    this.setState({ [name]: value })
   }
 
   render() {
     return (
       <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field">
-              <input type="text" id="enter-task" />
-              <label htmlFor="enter-task">Enter a Task</label>
+        <div className="col s12 l6 offset-l3">
+          <div className="card">
+            <div className="card-content">
+              <span className="card-title center">Todo App</span>
+              <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                  <div className="input-field">
+                    <input
+                      type="text"
+                      id="enter-task"
+                      name="task"
+                      value={this.state.task}
+                      onChange={this.handleChange}
+                    />
+                    <label htmlFor="enter-task">Enter a Task</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field">
+                    <input
+                      type="text"
+                      id="enter-date"
+                      name="dueDate"
+                      value={this.state.dueDate}
+                      onChange={this.handleChange}
+                    />
+                    <label htmlFor="enter-date">Enter Due Date</label>
+                  </div>
+                </div>
+                <div className="row center">
+                  <button
+                    className="btn waves-effect waves-light"
+                    type="submit"
+                    name="action"
+                  >
+                    Add Todo
+                    <i className="material-icons right">send</i>
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     )
   }
